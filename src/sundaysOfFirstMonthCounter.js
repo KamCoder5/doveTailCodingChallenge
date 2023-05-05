9c5bc4f0199e881e9f6d11ac54d3de526b44ec20
KamCoder5 <KamilahMohchin@gmail.com>
2023-05-05 16:37:33 +0100

    added test

--- src/sundaysOfFirstMonthCounter.js
+++ src/sundaysOfFirstMonthCounter.js
@@ -1,1 +1,4 @@
 const sundaysOfFirstMonthCounter = require("./sundaysOfFirstMonthCounter.js");
+test("should return a number", () => {
+  expect(typeof sundaysOfFirstMonthCounter()).toBe("number");
+});
--- src/sundaysOfFirstMonthCounter.test.js
+++ src/sundaysOfFirstMonthCounter.test.js
@@ -1,3 +1,3 @@
 const sundaysOfFirstMonthCounter = () => {
-  console.log("Sunday");
+  return 2;
 };

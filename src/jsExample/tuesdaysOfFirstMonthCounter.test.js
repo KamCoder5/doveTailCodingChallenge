bde9ca2dcbede4356c1b6f06201bedb5df42e053
KamCoder5 <KamilahMohchin@gmail.com>
2023-05-09 17:26:03 +0100

    added tests to tuesdayOfFirstMonthCounter

--- src/jsExample/tuesdaysOfFirstMonthCounter.test.js
+++ src/jsExample/tuesdaysOfFirstMonthCounter.test.js
@@ -1,11 +1,26 @@
 const tuesdaysOfFirstMonthCounter = require("./tuesdaysOfFirstMonthCounter");
 
 describe("tuesdaysOfFirstMonthCounter", () => {
-  it("returns the correct count when called", () => {
-    expect(tuesdaysOfFirstMonthCounter()).toBe(171);
+  it("returns the correct count when called for a different century", () => {
+    expect(tuesdaysOfFirstMonthCounter(1801, 1900)).toBe(171);
   });
 
-  it("returns a number value", () => {
-    expect(typeof tuesdaysOfFirstMonthCounter()).toBe("number");
+  it("returns 0 if no Tuesdays fall on the first of the month", () => {
+    const mockGetDay = jest.spyOn(Date.prototype, "getDay");
+    mockGetDay.mockReturnValueOnce(0); // set the first day of the month to a Sunday
+    expect(tuesdaysOfFirstMonthCounter()).toBe(0);
+    mockGetDay.mockRestore(); // restore the original Date.prototype.getDay() method
+  });
+
+  it("returns the correct count when no years are provided", () => {
+    expect(tuesdaysOfFirstMonthCounter(undefined, undefined)).toBe(171);
+  });
+
+  it("returns 0 if the years are invalid", () => {
+    expect(tuesdaysOfFirstMonthCounter("abcd", "efgh")).toBe(0);
+  });
+
+  it("returns 0 if the years are in reverse order", () => {
+    expect(tuesdaysOfFirstMonthCounter(2000, 1901)).toBe(0);
   });
 });
